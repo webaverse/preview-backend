589b20d5c6a6bdbe2ccd223c58e9773eeaf51f05
Avaer Kazmer <a@modules.io>
2021-01-14 20:50:41 -0500

    Pass through e argument to renderer

--- routes/land-preview.js
+++ routes/land-preview.js
@@ -159,7 +159,7 @@ const _handleLandPreviewRequest = async (req, res) => {
 
         await Promise.race([
           (async () => {
-            await page.goto(`https://app.webaverse.com/map.html?x=${x}&y=${y}&sw=${z}&sh=${z}&dw=512&dh=512&dst=http://${PREVIEW_HOST}:${PREVIEW_PORT}/` + index);
+            await page.goto(`https://app.webaverse.com/map.html?x=${x}&y=${y}&sw=${z}&sh=${z}&dw=512&dh=512${e ? `&e=${e}` : ''}&dst=http://${PREVIEW_HOST}:${PREVIEW_PORT}/` + index);
             const {
               req: proxyReq,
               res: proxyRes,
