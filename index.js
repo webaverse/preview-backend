164a940fd2b35726304262ed565fec810a48c292
shawticus <shawmakesmagic@gmail.com>
2021-03-23 19:09:09 -0700

    Handle preview-backend requests for testing

--- index.js
+++ index.js
@@ -2,42 +2,12 @@ const path = require('path');
 const stream = require('stream');
 const fs = require('fs');
 const url = require('url');
-// const querystring = require('querystring');
 const http = require('http');
 const https = require('https');
-// const dns = require('dns');
-// const crypto = require('crypto');
-// const zlib = require('zlib');
-// const child_process = require('child_process');
-// const mkdirp = require('mkdirp');
-// const express = require('express');
-// const httpProxy = require('http-proxy');
-// const ws = require('ws');
-// const LRU = require('lru');
-// const request = require('request');
-// const mime = require('mime');
-// const AWS = require('aws-sdk');
-// const Stripe = require('stripe');
-// const puppeteer = require('puppeteer');
-// const namegen = require('./namegen.js');
-// const Base64Encoder = require('./encoder.js').Encoder;
-// const {JSONServer, CustomEvent} = require('./dist/sync-server.js');
-// const fetch = require('node-fetch');
-// const {SHA3} = require('sha3');
-// const {default: formurlencoded} = require('form-urlencoded');
-// const Web3 = require('web3');
-// const bip39 = require('bip39');
-// const {hdkey} = require('ethereumjs-wallet');
-// const blockchain = require('./blockchain.js');
-// const {getExt, makePromise} = require('./utils.js');
-// const accountManager = require('./account-manager.js');
-// const eventsManager = require('./events-manager.js');
-// const ethereumHost = 'ethereum.exokit.org';
-
-// const api = require('./api.js');
 const { _handlePreviewRequest } = require('./routes/preview.js')
 const { _handleLandPreviewRequest } = require('./routes/land-preview.js')
 const { _handleBakeRequest } = require('./routes/bake.js')
+
 let CERT, PRIVKEY
 try {
   CERT = fs.readFileSync('./certs/fullchain.pem');
@@ -58,6 +28,9 @@ const _req = protocol => (req, res) => {
       return;
     } else if (o.host === 'bake.exokit.org') {
       _handleBakeRequest(req, res);
+      return;
+    } else if (o.host.includes("preview-backend")){
+      _handlePreviewRequest(req, res);
       return;
     }
 
