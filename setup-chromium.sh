af6b47be77a4ff2e2bcfef85e19a552f7185da19
shawticus <shawmakesmagic@gmail.com>
2021-04-14 14:01:24 -0700

    fix eb deps

--- setup-chromium.sh
+++ setup-chromium.sh
@@ -13,14 +13,11 @@ echo
 echo "~~~~~~ always good to do a yum update"
 yum update -y
 
-git --version 
-
 echo
 echo "~~~~~~ setup extras needed for other tools"
 echo
 yum install -y gcc openssl-devel bzip2-devel libffi-devel 
 sudo amazon-linux-extras install epel -y
-
 
 echo
 echo "~~~~~~ setup vm desktop and web browser"
@@ -34,6 +31,4 @@ sudo yum install chromium -y
 yum install xorg-x11-xauth -y
 yum install xclock xterm -y
 
-# clean up file
-rm -Rf ~/dev/demo/google-chrome-stable_current_x86_64.rpm
 echo "********* INSTALLED"
