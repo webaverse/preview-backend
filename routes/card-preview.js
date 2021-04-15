96d2fd128ab28fba3a3b39c3c35acabf66f9669c
Avaer Kazmer <a@modules.io>
2021-04-15 01:18:09 -0400

    Create card-preview.js

---
+++ routes/card-preview.js
@@ -0,0 +1,189 @@
+const url = require('url');
+const http = require('http');
+const mime = require('mime');
+
+const {getObject, putObject} = require('../aws.js');
+const browserManager = require('../browser-manager.js');
+
+const PREVIEW_HOST = '127.0.0.1';
+const PREVIEW_PORT = 8996;
+
+const bucketNames = {
+  preview: 'preview-exokit-org',
+};
+const storageHost = 'https://ipfs.exokit.org';
+
+const _makePromise = () => {
+  let accept, reject;
+  const p = new Promise((a, r) => {
+    accept = a;
+    reject = r;
+  });
+  p.accept = accept;
+  p.reject = reject;
+  return p;
+};
+
+const _warn = err => {
+  console.warn('uncaught: ' + err.stack);
+};
+process.on('uncaughtException', _warn);
+process.on('unhandledRejection', _warn);
+
+let browser;
+const serverPromise = _makePromise();
+let cbIndex = 0;
+const cbs = {};
+
+(async () => {
+browser = await browserManager.getBrowser();
+ticketManager = browserManager.makeTicketManager(4);
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  if (req.method === 'OPTIONS') {
+    res.end();
+  } else if (req.method === 'POST') {
+    const match = req.url.match(/^\/([0-9]+)/);
+    // console.log('callback server 1', req.url, !!match);
+    if (match) {
+      const index = parseInt(match[1], 10);
+      const cb = cbs[index];
+      // console.log('callback server 2', req.url, index, !!cb);
+      if (cb) {
+        delete cbs[index];
+        cb({req, res});
+      } else {
+        res.statusCode = 404;
+        res.end();
+      }
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+server.on('error', serverPromise.reject.bind(serverPromise));
+server.listen(PREVIEW_PORT, PREVIEW_HOST, serverPromise.accept.bind(serverPromise));
+})();
+
+const handleCardPreviewRequest = async (req, res) => {
+  await serverPromise;
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+
+  const u = url.parse(req.url, true);
+  const match = u.pathname.match(/^\/([0-9]+)$/);
+  const {query = {}} = u;
+  const cache = !query['nocache'];
+  if (match) {
+    const key = `cards/${tokenId}`;
+    const o = cache ? await (async () => {
+      try {
+        return await getObject(
+          bucketNames.preview,
+          key,
+        );
+      } catch(err) {
+        // console.warn(err);
+        return null;
+      }
+    })() : null;
+    const contentType = mime.getType(ext);
+    if (o) {
+      // res.setHeader('Content-Type', o.ContentType || 'application/octet-stream');
+      res.setHeader('Content-Type', contentType);
+      res.end(o.Body);
+    } else {
+      await ticketManager.lock();
+
+      const p = _makePromise()
+      const index = ++cbIndex;
+      cbs[index] = p.accept.bind(p);
+
+      let page;
+      try {
+        // console.log('preview 3');
+        page = await browser.newPage();
+        // console.log('preview 4');
+        page.on('console', e => {
+          console.log(e);
+        });
+        page.on('error', err => {
+          console.log(err);
+        });
+        page.on('pageerror', err => {
+          console.log(err);
+        });
+
+        let timeout;
+        const t = new Promise((accept, reject) => {
+          timeout = setTimeout(() => {
+            reject(new Error('timed out'));
+          }, 10 * 1000);
+        });
+
+        await Promise.race([
+          (async () => {
+            await page.goto(`https://cards.webaverse.com/?t=${tokenId}`);
+            const {
+              req: proxyReq,
+              res: proxyRes,
+            } = await p;
+
+            res.setHeader('Content-Type', contentType);
+            proxyReq.pipe(res);
+
+            const bs = [];
+            proxyReq.on('data', d => {
+              bs.push(d);
+            });
+            proxyReq.on('error', err => {
+              console.warn(err);
+            });
+            await new Promise((accept, reject) => {
+              proxyReq.on('end', accept);
+            });
+            proxyRes.end();
+            // page.close();
+
+            if (cache) {
+              const b = Buffer.concat(bs);
+              bs.length = 0;
+              await putObject(
+                bucketNames.preview,
+                key,
+                b,
+                contentType,
+              );
+            }
+          })(),
+          t,
+        ]);
+        clearTimeout(timeout);
+      } catch (err) {
+        console.warn(err.stack);
+      } finally {
+        ticketManager.unlock();
+
+        if (page) {
+          page.close();
+        }
+      }
+    }
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+};
+
+module.exports = {
+  _handleCardPreviewRequest,
+}
