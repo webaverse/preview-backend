e3e4b158192104e4e8ca047c970e2ee2c23e649e
Avaer Kazmer <a@modules.io>
2021-05-01 18:23:07 -0400

    Update card-preview.js

--- routes/card-preview.js
+++ routes/card-preview.js
@@ -173,7 +173,7 @@ const _handleCardPreviewRequest = async (req, res) => {
         }
       }
     } else if (req.method === 'DELETE') {
-      console.log('preview cards delete request', {hash, ext, type, cache, height, width, key});
+      console.log('preview cards delete request', {hash, ext, key});
       
       await deleteObject(
         bucketNames.preview,
