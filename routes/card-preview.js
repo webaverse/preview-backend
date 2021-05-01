3ff27bf9609ad982de9b96621b201d73ba4ca12e
Avaer Kazmer <a@modules.io>
2021-05-01 18:21:27 -0400

    Update card-preview.js

--- routes/card-preview.js
+++ routes/card-preview.js
@@ -2,7 +2,7 @@ const url = require('url');
 const http = require('http');
 const mime = require('mime');
 
-const {getObject, putObject} = require('../aws.js');
+const {getObject, putObject, deleteObject} = require('../aws.js');
 const browserManager = require('../browser-manager.js');
 const {renderTimeout} = require('../constants.js');
 
@@ -48,127 +48,147 @@ const _handleCardPreviewRequest = async (req, res) => {
   const {w = 500 + '', ext = 'png'} = query;
   const cardWidth = parseInt(w, 10);
   if (!isNaN(tokenId) && ['png', 'jpg'].includes(ext) && !isNaN(cardWidth)) {
-    const cardHeight = cardWidth / 2.5 * 3.5;
-    const cache = !query['nocache'];
-    
-    console.log('card preview request', {tokenId, ext, w, cardWidth, cardHeight});
-    
     const key = `cards/${tokenId}/${ext}`;
-    const o = cache ? await (async () => {
-      try {
-        return await getObject(
-          bucketNames.preview,
-          key,
-        );
-      } catch(err) {
-        // console.warn(err);
-        return null;
-      }
-    })() : null;
-    const contentType = mime.getType(ext);
-    if (o) {
-      // res.setHeader('Content-Type', o.ContentType || 'application/octet-stream');
-      res.setHeader('Content-Type', contentType);
-      res.setHeader('ETag', o.ETag);
-      res.end(o.Body);
-    } else {
-      await ticketManager.lock();
+    
+    if (req.method === 'GET') {    
+      const cardHeight = cardWidth / 2.5 * 3.5;
+      const cache = !query['nocache'];
+      
+      console.log('card preview get request', {tokenId, ext, w, cardWidth, cardHeight});
+      
+      const o = cache ? await (async () => {
+        try {
+          return await getObject(
+            bucketNames.preview,
+            key,
+          );
+        } catch(err) {
+          // console.warn(err);
+          return null;
+        }
+      })() : null;
+      const contentType = mime.getType(ext);
+      if (o) {
+        // res.setHeader('Content-Type', o.ContentType || 'application/octet-stream');
+        res.setHeader('Content-Type', contentType);
+        res.setHeader('ETag', o.ETag);
+        res.end(o.Body);
+      } else {
+        await ticketManager.lock();
 
-      let page;
-      try {
-        page = await browser.newPage();
-        // const p = _makePromise();
-        page.on('console', e => {
-          console.log(e);
-          /* const text = e.text();
-          if (/cards done render/i.test(text)) {
-            p.accept();
-          } */
-        });
-        page.on('error', err => {
-          console.log(err);
-        });
-        page.on('pageerror', err => {
-          console.log(err);
-        });
+        let page;
+        try {
+          page = await browser.newPage();
+          // const p = _makePromise();
+          page.on('console', e => {
+            console.log(e);
+            /* const text = e.text();
+            if (/cards done render/i.test(text)) {
+              p.accept();
+            } */
+          });
+          page.on('error', err => {
+            console.log(err);
+          });
+          page.on('pageerror', err => {
+            console.log(err);
+          });
 
-        let timeout;
-        const t = new Promise((accept, reject) => {
-          timeout = setTimeout(() => {
-            reject(new Error('timed out: ' + JSON.stringify({tokenId, ext, w, cardWidth, cardHeight}, null, 2)));
-          }, renderTimeout);
-        });
+          let timeout;
+          const t = new Promise((accept, reject) => {
+            timeout = setTimeout(() => {
+              reject(new Error('timed out: ' + JSON.stringify({tokenId, ext, w, cardWidth, cardHeight}, null, 2)));
+            }, renderTimeout);
+          });
 
-        await Promise.race([
-          (async () => {
-            console.log('load page 1');
-            await page.setViewport({
-              width: cardWidth,
-              height: cardHeight,
-              // deviceScaleFactor: 1,
-            });
-            /* await page.exposeFunction('onMessageReceivedEvent', e => {
-              console.log('got event outer', e);
-              p.accept(e.data);
-            }); */
-            console.log('load page 2');
-            await page.goto(`https://cards.webaverse.com/?t=${tokenId}&w=${cardWidth}`, {
-              waitUntil: 'networkidle0',
-            });
-            console.log('load page 3');
-            
-            /* function listenFor(type) {
-              return page.evaluateOnNewDocument(type => {
-                console.log('add listener', type, !!window.onMessageReceivedEvent);
-                window.addEventListener(type, e => {
-                  console.log('got event inner', type, !!window.onMessageReceivedEvent, e);
-                  window.onMessageReceivedEvent({type, data: e.data});
-                });
-              }, type);
-            }
-            await listenFor('message'); // Listen for "message" custom event on page load. */
-            
-            console.log('load page 4');
-            
-            // await p;
-            
-            console.log('load page 5');
-            
-            const b = await page.screenshot({
-              type: (() => {
-                switch (ext) {
-                  case 'png': return 'png';
-                  case 'jpg': return 'jpeg';
-                  default: return null;
-                }
-              })(),
-              omitBackground: true,
-            });
-            console.log('load page 6');
-            
-            res.setHeader('Content-Type', contentType);
-            res.end(b);
+          await Promise.race([
+            (async () => {
+              console.log('load page 1');
+              await page.setViewport({
+                width: cardWidth,
+                height: cardHeight,
+                // deviceScaleFactor: 1,
+              });
+              /* await page.exposeFunction('onMessageReceivedEvent', e => {
+                console.log('got event outer', e);
+                p.accept(e.data);
+              }); */
+              console.log('load page 2');
+              await page.goto(`https://cards.webaverse.com/?t=${tokenId}&w=${cardWidth}`, {
+                waitUntil: 'networkidle0',
+              });
+              console.log('load page 3');
+              
+              /* function listenFor(type) {
+                return page.evaluateOnNewDocument(type => {
+                  console.log('add listener', type, !!window.onMessageReceivedEvent);
+                  window.addEventListener(type, e => {
+                    console.log('got event inner', type, !!window.onMessageReceivedEvent, e);
+                    window.onMessageReceivedEvent({type, data: e.data});
+                  });
+                }, type);
+              }
+              await listenFor('message'); // Listen for "message" custom event on page load. */
+              
+              console.log('load page 4');
+              
+              // await p;
+              
+              console.log('load page 5');
+              
+              const b = await page.screenshot({
+                type: (() => {
+                  switch (ext) {
+                    case 'png': return 'png';
+                    case 'jpg': return 'jpeg';
+                    default: return null;
+                  }
+                })(),
+                omitBackground: true,
+              });
+              console.log('load page 6');
+              
+              res.setHeader('Content-Type', contentType);
+              res.end(b);
 
-            if (cache) {
-              await putObject(
-                bucketNames.preview,
-                key,
-                b,
-                contentType,
-              );
-            }
-          })(),
-          t,
-        ]);
-      } catch (err) {
-        console.warn(err.stack);
-      } finally {
-        ticketManager.unlock();
+              if (cache) {
+                await putObject(
+                  bucketNames.preview,
+                  key,
+                  b,
+                  contentType,
+                );
+              }
+            })(),
+            t,
+          ]);
+        } catch (err) {
+          console.warn(err.stack);
+        } finally {
+          ticketManager.unlock();
 
-        if (page) {
-          page.close();
+          if (page) {
+            page.close();
+          }
         }
       }
+    } else if (req.method === 'DELETE') {
+      console.log('preview cards delete request', {hash, ext, type, cache, height, width, key});
+      
+      await deleteObject(
+        bucketNames.preview,
+        key
+      );
+      
+      res.setHeader('Content-Type', 'application/json');
+      const j = {
+        ok: true,
+      };
+      const s = JSON.stringify(j);
+      res.end(s);
+    } else {
+      res.statusCode = 404;
+      res.end();
     }
   } else {
     res.statusCode = 404;
