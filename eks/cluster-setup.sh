15b6137eaaa28187f05c9818f06986b13f4c27e2
Muhammad Abeer <abeersaqib948@gmail.com>
2021-11-12 03:28:09 +0500

    Fixes2

--- eks/cluster-setup.sh
+++ eks/cluster-setup.sh
@@ -1,6 +1,6 @@
 echo "Inside cluster setup"
 echo $AWS_DEFAULT_REGION
 #Create cluster if don't exist
-aws ec2 describe-instance-type-offerings --location-type availability-zone  --filters Name=instance-type,Values=p2.xlarge --region us-west-1 --output table
+#aws ec2 describe-instance-type-offerings --location-type availability-zone  --filters Name=instance-type,Values=p2.xlarge --region us-west-1 --output table
 #eksctl delete cluster --name=preview-server  --wait || echo "No Cluster named Preview Server"
 aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name preview-server || eksctl create cluster -f ./eks/cluster.yaml
