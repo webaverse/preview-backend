425bf6a27add09dae1e6bb22d6bda30b53dec934
Muhammad Abeer <abeersaqib948@gmail.com>
2021-11-12 00:10:26 +0500

    Fixes

--- eks/cluster-setup.sh
+++ eks/cluster-setup.sh
@@ -1,4 +1,4 @@
 echo "Inside cluster setup"
 ls -l
 #Create cluster if don't exist
-aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name preview-server || eksctl create cluster -f cluster.yaml
+aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name preview-server || eksctl create cluster -f ./eks/cluster.yaml
