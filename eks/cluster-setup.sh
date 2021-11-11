c8ddd19a54b877b5d3cfd1da443dc5df3ebca79d
Muhammad Abeer <abeersaqib948@gmail.com>
2021-11-12 00:57:10 +0500

    Added Nvidia and Auto Scaling Group

--- eks/cluster-setup.sh
+++ eks/cluster-setup.sh
@@ -1,4 +1,5 @@
 echo "Inside cluster setup"
 ls -l
 #Create cluster if don't exist
+eksctl delete cluster --name=preview-server  --wait
 aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name preview-server || eksctl create cluster -f ./eks/cluster.yaml
